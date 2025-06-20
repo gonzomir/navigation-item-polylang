fb8de25b30dccc6d82cce4171309473e90557020
Milen Petrinski <gonzo@greatgonzo.net>
2025-06-20 15:32:35 +0300

    Initial commit. Scafold the plugin and block files.

---
+++ plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Navigation Menu Item For Polylang
+ * Plugin URI: https://github.com/gonzomir/navigation-item-polylang
+ * Description: Adds Polylang language switcher support to WordPress navigation menus using the block editor.
+ * Version: 0.0.1
+ * Author: Milen Petrinski - Gonzo
+ * Author URI: https://greatgonzo.net
+ * Requires at least: 6.7
+ * License: GPL-2.0-or-later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: navigation-item-polylang
+ */
+
+namespace Gonzo\NavigationItemPolylang;
+
+require __DIR__ . '/inc/namespace.php';
+
+bootstrap();
